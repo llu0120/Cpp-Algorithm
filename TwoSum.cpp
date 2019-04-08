26fdabd8d08c29cbb49fa36514ceffae8c57ced6
llu0120 <41609738+llu0120@users.noreply.github.com>
2019-04-08 12:26:37 -0700

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,3 +1,4 @@
+//Hash Map
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //Example:
