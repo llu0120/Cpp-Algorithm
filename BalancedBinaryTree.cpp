126d10e16061b482b3c860f1589b040cab91085d
llu0120 <41609738+llu0120@users.noreply.github.com>
2019-04-09 23:34:10 -0700

    Create BalancedBinaryTree.cpp

---
+++ BalancedBinaryTree.cpp
@@ -0,0 +1,58 @@
+//DFS
+/**
+* Given a binary tree, determine if it is height-balanced.
+* For this problem, a height-balanced binary tree is defined as:
+* a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+* Example 1:
+* Given the following tree [3,9,20,null,null,15,7]:
+*    3
+*    / \
+*   9  20
+*     /  \
+*    15   7
+* Return true.
+*
+* Example 2:
+* 
+* Given the following tree [1,2,2,3,3,null,null,4,4]:
+* 
+*        1
+*       / \
+*      2   2
+*     / \
+*    3   3
+*   / \
+*  4   4
+* Return false.
+*/
+ 
+ 
+
+ 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (computeDepth(root) == -1) return false;
+        else return true;
+    }
+    int computeDepth(TreeNode* root){
+        if (!root) return 0; 
+        int left = computeDepth(root->left);
+        if (left == -1) return -1; 
+        int right = computeDepth(root->right);
+        if (right == -1) return -1; 
+        
+        if (abs(left-right)>1) return -1;
+        else return 1 + max(left,right);
+        
+    }
+};
