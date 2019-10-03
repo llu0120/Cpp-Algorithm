0384cba921253b9e64f502fc9788fc9db56533f9
llu-0120 <llu@eng.ucsd.edu>
2019-10-02 23:02:25 -0700

    Add ReverseString.cpp

---
+++ TwoPointer/ReverseString.cpp
@@ -0,0 +1,34 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+//right left pointer and swap O(n/2)
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0; 
+        int right = s.size() - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+        return;
+    }
+};
