eda724e10d287edb78c22136a55ce9820e0e28f4
llu-0120 <llu@eng.ucsd.edu>
2019-10-24 15:35:24 -0700

    Add MinStack.cpp

---
+++ DataStructure/MinStack.cpp
@@ -0,0 +1,62 @@
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+*/
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    //Use the second stack to record the minimum number
+    void push(int x) {
+        s1.push(x);
+        if (s2.empty() || s2.top() >= x) {
+            s2.push(x);
+        }
+    }
+    
+    void pop() {
+        if (s1.top() == s2.top()) {
+            s2.pop();
+        }
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+private: 
+    stack<int> s1, s2;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+i
